f6a8386859f2b6a33be6e5fbff3dac30a38aa74e
Andrea Balotti <andrea.balotti@hotmail.it>
2016-09-23 16:07:38 +0200

    Some fix on the unit and test example

--- R/vanGenuchten_swc.R
+++ R/vanGenuchten_swc.R
@@ -5,27 +5,43 @@
 #               Andrea Balotti - balotti.and at gmail.com
 #                   inversion and integration of geotop range limit
 #
-# Copyright (c), 2016 - Andrea Balotti
+# Copyright (c) 2016 - Andrea Balotti
 ################################################################################
 
 ### NOTE
 # van Genuchten inversion taken from GEOtop model "pedo.funct.c" function
 # https://github.com/geotopmodel
+# By default the function compute SWC from SWP, using "inv=TRUE" will be computed
+# the opposite.
 # Saturation condition is considered same as unsatured in this script because
-# otherwise specific storativity (Ss) parameter is also required. 
+# otherwise specific storativity (Ss) parameter is also required.
+# Conversion between column water and pressure:
+#   1 mm = 0.0980665 hPa / 0.00980665 kPa
+#   1 cm = 0.980665 hPa / 0.0980665 kPa
 
-### VARIABLE
-# swc           SWC (water content)     [m^3/m^3]   [0 < x < 1] -> non-% value
-# psi           SWP (suction pressure)  [cm]        [<= 0]
+### VARIABLE (the measure unit are to check !!!)
+# swc           SWC (water content)     [cm^3/cm^3] [0 < x < 1] -> non-% value
+# psi           SWP (pressure head)     [cm]        [<= 0] -> forced to absolute value in computation
 # alpha         air entry suction       [cm^-1]     [> 0]
 # n             pore-size distribution  [-]         [> 1]
-# theta_res     residual water content  [m^3/m^3]   [> 0]
-# theta_sat     saturated water content [m^3/m^3]   [> 0]
+# theta_res     residual water content  [cm^3/cm^3] [> 0]
+# theta_sat     saturated water content [cm^3/cm^3] [> 0]
 
 vanGenuchten_swc <- function(psi=NULL, swc=NULL, alpha, n, theta_res, theta_sat, inv = FALSE)
 {
+    # # convert input in numeric array
+    # for (i in c(psi, swc, alpha, n, theta_res, theta_sat)) {
+    #     if (!is.null(i)) as.numeric(i)
+    #     # print(str(i))
+    # }
+    # 
+    print(c(alpha,n,theta_res,theta_sat))
+    
+    # constant
     m <- 1-(1/n)        # utility variable
-    pmin <- -1*10^10    # minimum SWP value [Pa]
+    pmin <- -1*10^10    # minimum SWP value [Pa] (10^10 [Pa] = 9806650 [cm])
+    ss <- 10^-6         # specific storativity (correction for saturated case),
+                        # maybe, could be omitted in our case
     
     if (inv) {
     # compute SWP from SWC
@@ -52,19 +68,22 @@ vanGenuchten_swc <- function(psi=NULL, swc=NULL, alpha, n, theta_res, theta_sat,
             TETA[TETA<TETA_min] <- TETA_min
             
             # INVERSION FORMULA
-            # geotop inversion (return negative SWP) [Pa/hPa/cm ???]
+            # geotop inversion (return negative SWP) [cm]
             # (does square of negative value (NaN) if SWC is a percent value!!!)
             out <- ( ( (TETA^(-1/m))-1 )^(1/n) )*(-1/alpha)
+
             # force lower SWP value 
             out[TETA > 1-10^(-6)] <- 0
             
+            ### TO CHECK AND REMOVE ######################
             # if (geotop) {
                 # geotop inversion (return negative SWP)
                 # out <- ( ( (TETA^(-1/m))-1 )^(1/n) )*(-1/alpha)
             # } else {
                 # handmade inversion (return positive SWP) - DOESN'T WORK WELL
-                # out <- ( ( (exp(log(Theta)/m)-1)/alpha )*(1/n) )
+                # out <- ( ( (exp(log(TETA)/m)-1)/alpha )*(1/n) )
             # }
+            ##############################################
         }
 
     } else {
@@ -97,17 +116,24 @@ vanGenuchten_swc <- function(psi=NULL, swc=NULL, alpha, n, theta_res, theta_sat,
 }
 
 ### TEST CODE
-# alpha = c(1.333047,1.266704)
-# n = c(1.388409,1.398214)
-# theta_res = c(0.05,0.05)
-# theta_sat = c(0.5481991,0.5482869)
-# psi <- c(seq(1,100000,5),seq(1,100000,5))
-# psi <- -psi+sin(rnorm(length(psi)))
-# k=1
-# swc = vanGenuchten_swc(psi = psi, alpha = alpha[k], n = n[k],
-#                        theta_sat = theta_sat[k], theta_res = theta_res[k])
-# psi2 = vanGenuchten_swc(swc = swc, alpha = alpha[k], n = n[k],
-#                         theta_sat = theta_sat[k], theta_res = theta_res[k], inv = T)
-# plot(log(abs(psi),base = 10),swc)
-# plot(log(abs(psi2),base = 10),swc)
+alpha = c(0.005,1.333047,1.266704)
+n = c(2,1.388409,1.398214)
+theta_res = c(0.1,0.05,0.05)
+theta_sat = c(0.5,0.5481991,0.5482869)
+swc <- runif(1000,0.1784,0.5) # put same limit as out_swc
+psi <- runif(1000,min=0,max=1000)
+k=1
+out_swc = vanGenuchten_swc(psi = psi, alpha = alpha[k], n = n[k],
+                       theta_sat = theta_sat[k], theta_res = theta_res[k])
+out_psi = vanGenuchten_swc(swc = swc, alpha = alpha[k], n = n[k],
+                        theta_sat = theta_sat[k], theta_res = theta_res[k], inv = T)
+plot(abs(psi),out_swc)
+plot(abs(out_psi),swc)
+# plot(psi)
+# plot(swc)
+# # plot pedofunction
+plot(swc,abs(psi),log="y")
+plot(out_swc,abs(out_psi),log="y")
+# # plot pF between 0-7
+# plot(swc,log((abs(psi)),base = 10),ylim=c(0,7))
 ###
